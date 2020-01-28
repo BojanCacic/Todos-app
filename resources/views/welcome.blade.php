ff3a6400261ef7dbd73ddc08b3029c9519b8bbb1
BojanCacic <bojan.cacic.prijedor@gmail.com>
2020-01-28 19:19:15 +0100

    Setting up todos-app

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    <a href="todos">Todos List</a>
                 </div>
 
                 <div class="links">
